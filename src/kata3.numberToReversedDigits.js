b09b7eb4fce97536a9e5fdb2bee8a440b20fd1a1
Sofia <sofia.dionisio94@gmail.com>
2020-03-10 16:00:01 +0000

    changes

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,9 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = number => {
+  return number
+    .toString() // converts the number to a string
+    .split("") // splits the string to an array
+    .reverse() // reverses the array
+    .map(Number); // applies a function to each element in the array the function converts a string to an object
+};
 
 module.exports = numberToReversedDigits;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,11 @@
 const { humanCatDogYears } = require("../src");
 
 // Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+  test("returns array of human, cat and dog years when passed human years", () => {
+    expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+    expect(humanCatDogYears(11)).toEqual([11, 60, 69]);
+    expect(humanCatDogYears(5)).toEqual([5, 36, 39]);
+    expect(humanCatDogYears(3)).toEqual([3, 28, 29]);
+  });
+});
